426d296d40e281bcf15d400135b4453949184209
Jenkins <jenkins@kaltura.com>
2018-08-27 11:08:50 +0000

    Auto-generated by Jenkins

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -582,8 +582,8 @@ class Client extends Base
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-08-26');
-		$this->setApiVersion('5.0.1.43122');
+		$this->setClientTag('php5:18-08-27');
+		$this->setApiVersion('5.0.1.20006');
 	}
 	
 	/**
