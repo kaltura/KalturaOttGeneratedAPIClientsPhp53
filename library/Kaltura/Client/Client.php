8f485574325ab7c806e37df3fa98f4052bd9b8c8
Jenkins <jenkins@kaltura.com>
2018-08-29 12:32:21 +0000

    Auto-generated by Jenkins

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -577,8 +577,8 @@ class Client extends Base
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-08-20');
-		$this->setApiVersion('5.0.1.12280');
+		$this->setClientTag('php5:18-08-29');
+		$this->setApiVersion('5.0.1.22506');
 	}
 	
 	/**
--- library/Kaltura/Client/Type/RelatedFilter.php
+++ library/Kaltura/Client/Type/RelatedFilter.php
@@ -50,8 +50,6 @@ class RelatedFilter extends \Kaltura\Client\Type\BaseSearchAssetFilter
 		if(is_null($xml))
 			return;
 		
-		if(count($xml->kSql))
-			$this->kSql = (string)$xml->kSql;
 		if(count($xml->idEqual))
 			$this->idEqual = (int)$xml->idEqual;
 		if(count($xml->typeIn))
@@ -64,25 +62,6 @@ class RelatedFilter extends \Kaltura\Client\Type\BaseSearchAssetFilter
 				$this->excludeWatched = false;
 		}
 	}
-	/**
-	 * Search assets using dynamic criteria. Provided collection of nested expressions with key, comparison operators, value, and logical conjunction.
-	 *             Possible keys: any Tag or Meta defined in the system and the following reserved keys: start_date, end_date. 
-	 *             epg_id, media_id - for specific asset IDs.
-	 *             geo_block - only valid value is &quot;true&quot;: When enabled, only assets that are not restriced to the user by geo-block rules will return.
-	 *             parental_rules - only valid value is &quot;true&quot;: When enabled, only assets that the user doesn&#39;t need to provide PIN code will return.
-	 *             user_interests - only valid value is &quot;true&quot;. When enabled, only assets that the user defined as his interests (by tags and metas) will return.
-	 *             epg_channel_id – the channel identifier of the EPG program. *****Deprecated, please use linear_media_id instead*****
-	 *             linear_media_id – the linear media identifier of the EPG program.
-	 *             entitled_assets - valid values: &quot;free&quot;, &quot;entitled&quot;, &quot;both&quot;. free - gets only free to watch assets. entitled - only those that the user is implicitly entitled to watch.
-	 *             Comparison operators: for numerical fields =, &gt;, &gt;=, &lt;, &lt;=, : (in). 
-	 *             For alpha-numerical fields =, != (not), ~ (like), !~, ^ (any word starts with), ^= (phrase starts with), + (exists), !+ (not exists).
-	 *             Logical conjunction: and, or. 
-	 *             Search values are limited to 20 characters each.
-	 *             (maximum length of entire filter is 2048 characters)
-	 * @var string
-	 */
-	public $kSql = null;
-
 	/**
 	 * the ID of the asset for which to return related assets
 	 * @var int
