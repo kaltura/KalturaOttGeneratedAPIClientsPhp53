bcedf55f4cbbb42cc661a8a0898f7030739b856f
Jenkins <jenkins@kaltura.com>
2019-01-17 23:48:06 +0000

    Auto-generated by Jenkins

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -9,7 +9,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2018  Kaltura Inc.
+// Copyright (C) 2006-2019  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -608,7 +608,7 @@ class Client extends Base
 		parent::__construct($config);
 		
 		$this->setClientTag('php5:19-01-17');
-		$this->setApiVersion('5.1.1.24444');
+		$this->setApiVersion('5.1.1.42808');
 	}
 	
 	/**
