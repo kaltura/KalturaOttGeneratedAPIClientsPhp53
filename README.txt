2352efc1abe46832a8945e221ba1cdd3bc34bb75
Backend CI <ott.rnd.core@kaltura.com>
2019-12-17 14:08:11 +0000

    Auto-generated by Jenkins

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP 5.3 API Client Library for usage with namespace.
-Compatible with Kaltura server version 5.2.3.5604 and above.
+Compatible with Kaltura server version 5.2.4.24184 and above.
--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -627,8 +627,8 @@ class Client extends Base
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:19-10-22');
-		$this->setApiVersion('5.2.3.5604');
+		$this->setClientTag('php5:19-12-17');
+		$this->setApiVersion('5.2.4.24184');
 	}
 	
 	/**
