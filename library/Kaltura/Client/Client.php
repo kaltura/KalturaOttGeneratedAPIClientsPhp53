0268f40877efc6f076216b57eac5a4ac37fb3903
Backend CI <ott.rnd.core@kaltura.com>
2019-12-17 19:06:57 +0000

    Auto-generated by Jenkins

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -653,7 +653,7 @@ class Client extends Base
 		parent::__construct($config);
 		
 		$this->setClientTag('php5:19-12-17');
-		$this->setApiVersion('5.2.8.14015');
+		$this->setApiVersion('5.2.8.14025');
 	}
 	
 	/**
