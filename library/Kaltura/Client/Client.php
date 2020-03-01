7add1ef3473b8774ee7ef69f338aeaff56539c01
Backend CI <ott.rnd.core@kaltura.com>
2020-03-01 10:01:34 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2719, branch master

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -658,7 +658,7 @@ class Client extends Base
 		parent::__construct($config);
 		
 		$this->setClientTag('php5:20-03-01');
-		$this->setApiVersion('5.3.2.14732');
+		$this->setApiVersion('5.3.2.14737');
 	}
 	
 	/**
