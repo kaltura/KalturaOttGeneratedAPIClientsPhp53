c2f25fede4ce983dff74b10e5a073cbd7e9373ee
Backend CI <ott.rnd.core@kaltura.com>
2020-09-01 12:55:03 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/158, branch 5_5_1

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP 5.3 API Client Library for usage with namespace.
-Compatible with Kaltura server version 5.5.1.28285 and above.
+Compatible with Kaltura server version 5.5.1.28288 and above.
--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -682,8 +682,8 @@ class Client extends Base
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:20-08-26');
-		$this->setApiVersion('5.5.1.28285');
+		$this->setClientTag('php5:20-09-01');
+		$this->setApiVersion('5.5.1.28288');
 	}
 	
 	/**
