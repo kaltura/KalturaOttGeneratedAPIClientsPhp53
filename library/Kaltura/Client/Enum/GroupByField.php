b71ce331758af97002346adc9612661ddb33d52a
Backend CI <ott.rnd.core@kaltura.com>
2021-04-19 12:42:01 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/440, branch master

--- library/Kaltura/Client/Enum/GroupByField.php
+++ library/Kaltura/Client/Enum/GroupByField.php
@@ -6,7 +6,7 @@
 //                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
 //
 // This file is part of the Kaltura Collaborative Media Suite which allows users
-// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
 // Copyright (C) 2006-2021  Kaltura Inc.
@@ -39,8 +39,5 @@ namespace Kaltura\Client\Enum;
 class GroupByField extends \Kaltura\Client\EnumBase
 {
 	const MEDIA_TYPE_ID = "media_type_id";
-	const SUPPRESSED = "suppressed";
-	const CRID = "crid";
-	const LINEAR_MEDIA_ID = "linear_media_id";
 }
 
